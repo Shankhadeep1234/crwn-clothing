3d6682f9e09cce396b2e86428c781931a0280b09
Shankhadeep Bhadra <shankhadeepraj@gmail.com>
2019-11-20 23:39:38 +0530

    Starting our sign in section by creating a new sign in and sign up page, as well as start creating sign in component.

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+import "./sign-in-and-sign-up.style.scss";
+
+const SignInAndSignUpPage = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUpPage;
